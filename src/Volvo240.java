e272d0f149a4087167fd2f3942a8a66ce3edd482
BjornRo <shovitswe@gmail.com>
2020-01-29 15:39:43 +0100

    Added comments to volvo240

--- src/Volvo240.java
+++ src/Volvo240.java
@@ -2,24 +2,41 @@ import java.awt.*;
 
 public class Volvo240 extends Car {
 
+    /**
+     * Increased amount of horsepowers, if you tune your engine.
+     */
     public final static double trimFactor = 1.25;
 
-
+    /**
+     * Creates a standard Volvo 240.
+     */
     public Volvo240() {
         super(4, 100, Color.BLACK, "Volovo240");
     }
 
 
+    /**
+     *
+     * @return Returns the difference in total speed.
+     */
     @Override
     protected double speedFactor() {
         return enginePower * 0.01 * trimFactor;
     }
 
+    /**
+     *
+     * @param amount Increases current speed.
+     */
     @Override
     public void incrementSpeed(double amount) {
         currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
     }
 
+    /**
+     *
+     * @param amount Decreases current speed.
+     */
     @Override
     public void decrementSpeed(double amount) {
         currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);

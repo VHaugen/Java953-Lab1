fef879d7ff28040c64870d40cb38a9de0ccd7ef8
Concrematio <the_proffs@yahoo.se>
2020-01-29 15:56:05 +0100

    added javadocumentation

--- src/Car.java
+++ src/Car.java
@@ -14,6 +14,16 @@ public abstract class Car implements Movable {
 
 
 
+    /**
+     * Returns an Image object that can then be painted on the screen.
+     * The url argument must specify an absolute . The name
+     * argument is a specifier that is relative to the url argument.
+     *
+     * @param  _nrDoors  The number of doors of this <code>Car</code>.
+     * @param  _enginePower  The power of the engine in BHP.
+     * @param  _color  The <code>Color</code> of this <code>Car</code>.
+     * @param  _modelName  The model name of this <code>Car</code>
+     */
     Car(int _nrDoors, double _enginePower, Color _color, String _modelName) {
         nrDoors = _nrDoors;
         enginePower = _enginePower;
@@ -22,37 +32,74 @@ public abstract class Car implements Movable {
         stopEngine();
     }
 
-
+    /**
+     * Gets the number of doors
+     *
+     * @return  The number of doors
+     */
     public int getNrDoors() {
         return nrDoors;
     }
 
+    /**
+     * Gets the engine power
+     *
+     * @return  The engine power
+     */
     public double getEnginePower() {
         return enginePower;
     }
 
+    /**
+     * Gets the current speed
+     *
+     * @return  The current speed
+     */
     public double getCurrentSpeed() {
         return currentSpeed;
     }
 
+    /**
+     * Gets the <code>Color</code>
+     *
+     * @return  The <code>Color</code>
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     *
+     * @param clr The new <code>Color</code> of this <code>Car</code>
+     */
     public void setColor(Color clr) {
         color = clr;
     }
 
+    /**
+     * Sets the current speed of this car to a low initial value
+     */
     public void startEngine() {
         currentSpeed = 0.1;
     }
 
+    /**
+     * Increments the speed of this <code>Car</code>.
+     * @param amount How much the speed will be incremented.
+     */
     abstract protected void incrementSpeed(double amount);
 
+    /**
+     * Decrements the speed of this <code>Car</code>.
+     * @param amount How much the speed will be Decremented.
+     */
     abstract protected void decrementSpeed(double amount);
 
     abstract protected double speedFactor();
 
+    /**
+     * Sets the current speed to 0.
+     */
     public void stopEngine() {
         currentSpeed = 0;
     }
@@ -67,18 +114,27 @@ public abstract class Car implements Movable {
         decrementSpeed(amount);
     }
 
+    /**
+     * Moves this <code>Car</code> in the current direction according to the current speed.
+     */
     public void move() {
         posX += velX * currentSpeed;
         posY += velY * currentSpeed;
 
     }
 
+    /**
+     * Changes the current direction 90° to the left.
+     */
     public void ternLeft() {
         double tempVel = velX;
         velX = velY;
         velY = -tempVel ;
     }
 
+    /**
+     * Changes the current direction 90° to the right.
+     */
     public void turnRight() {
         double tempVel = velX;
         velX = -velY;

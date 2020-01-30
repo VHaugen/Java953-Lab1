4ae609ffe3aaca3a07216d9d456b725c4e67e7e3
Concrematio <the_proffs@yahoo.se>
2020-01-30 16:29:12 +0100

    added test cases for full brake and full gas

--- test/Bilprovningen.java
+++ test/Bilprovningen.java
@@ -4,79 +4,90 @@ import static org.junit.Assert.assertTrue;
 
 
 public class Bilprovningen {
-    Volvo240 volvo;
+    Car testCar;
     Saab95 saab;
 
     @Before
     public void init() {
-        volvo = new Volvo240();
+        testCar = new Volvo240();
+        testCar.startEngine();
     }
 
     @Test
-    public void testStartEngineVolvo(){
-        volvo = new Volvo240();
-        volvo.startEngine();
-
-        assertTrue(volvo.currentSpeed > 0);
+    public void testStartEngineVolvo() {
+        assertTrue(testCar.currentSpeed > 0);
 
     }
 
     @Test
     public void testMove() {
-        double initialPosY = volvo.posY;
-        volvo.startEngine();
-        volvo.gas(1);
-        volvo.move();
+        double initialPosY = testCar.posY;
+        testCar.gas(1);
+        testCar.move();
 
-        assertTrue(volvo.posY > initialPosY);
+        assertTrue(testCar.posY > initialPosY);
     }
 
     @Test
     public void testGasVolvo(){
-        volvo.startEngine();
-        double initialSpeed = volvo.getCurrentSpeed();
+        double initialSpeed = testCar.getCurrentSpeed();
 
-        volvo.gas(1);
+        testCar.gas(1);
 
-        assertTrue(initialSpeed < volvo.getCurrentSpeed());
+        assertTrue(initialSpeed < testCar.getCurrentSpeed());
+
+    }
+    @Test
+    public void testFullGasVolvo(){
+        for (int i = 0; i < 200; i++) {
+            testCar.gas(1);
+        }
+        assertTrue(testCar.currentSpeed <= testCar.enginePower);
 
     }
 
     @Test
+    public void testFullBrake() {
+        for (int i = 0; i < 200; i++) {
+            testCar.brake(1);
+        }
+        assertTrue(testCar.currentSpeed >= 0);
+    }
+
+    @Test
     public void testBrakeVolvo(){
-        volvo.startEngine();
-        volvo.gas(1);
-        double initialSpeed = volvo.getCurrentSpeed();
+        testCar.gas(1);
+        double initialSpeed = testCar.getCurrentSpeed();
 
-        volvo.brake(1);
+        testCar.brake(1);
 
-        assertTrue(initialSpeed > volvo.getCurrentSpeed());
+        assertTrue(initialSpeed > testCar.getCurrentSpeed());
 
     }
 
     @Test
     public void testHandBrakeTurnLeft() {
-        volvo.velY = 1;
-        volvo.velX = 1;
+        testCar.velY = 1;
+        testCar.velX = 1;
 
-        volvo.turnLeft();
-        volvo.turnLeft();
+        testCar.turnLeft();
+        testCar.turnLeft();
 
-        assertTrue(volvo.velX == -1 && volvo.velY == -1);
+        assertTrue(testCar.velX == -1 && testCar.velY == -1);
 
 
     }
 
     @Test
     public void testSpinLeft() {
-        double initVelX = volvo.velX;
-        double initVelY = volvo.velY;
+        double initVelX = testCar.velX;
+        double initVelY = testCar.velY;
 
         for (int i = 0; i < 4; i++) {
-            volvo.turnLeft();
+            testCar.turnLeft();
         }
 
-        assertTrue(initVelX == volvo.velX && initVelY == volvo.velY);
+        assertTrue(initVelX == testCar.velX && initVelY == testCar.velY);
 
     }
 
@@ -84,12 +95,11 @@ public class Bilprovningen {
     public void testDriveToTheLeft() {
         double initialX = 0;
 
-        volvo.startEngine();
-        volvo.gas(1);
-        volvo.turnLeft();
-        volvo.move();
+        testCar.gas(1);
+        testCar.turnLeft();
+        testCar.move();
 
-        assertTrue(initialX > volvo.posX);
+        assertTrue(initialX > testCar.posX);
 
     }
 
@@ -97,12 +107,11 @@ public class Bilprovningen {
     public void testDriveToTheRight() {
         double initialX = 0;
 
-        volvo.startEngine();
-        volvo.gas(1);
-        volvo.turnRight();
-        volvo.move();
+        testCar.gas(1);
+        testCar.turnRight();
+        testCar.move();
 
-        assertTrue(initialX < volvo.posX);
+        assertTrue(initialX < testCar.posX);
     }
 
 }

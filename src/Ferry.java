ed1097596f805f4a3e35e670177dbf9b6dd65760
BjornRo <shovitswe@gmail.com>
2020-02-06 13:04:55 +0100

    Simplified ferry to one lane

--- src/Ferry.java
+++ src/Ferry.java
@@ -7,9 +7,8 @@ import java.util.Queue;
 public class Ferry extends Motorized implements ITransporter {
     private Bed bed;
     private int maxLaneLength;
-    private int numOfLanes;
-    private boolean[] isLaneFull;
-    private List<Queue<IFerry>> lanes = new ArrayList<>();
+    private boolean laneFull;
+    private Queue<IFerry> lane;
 
     /**
      * This constructor is generic and is made to take standard arguments
@@ -25,91 +24,65 @@ public class Ferry extends Motorized implements ITransporter {
     Ferry(double _enginePower, Color _color, String _modelName, int _maxLaneLength, int _numOfLanes) {
         super(_enginePower, _color, _modelName);
         maxLaneLength = _maxLaneLength;
-        numOfLanes = _numOfLanes;
         //Use to check if lane is full.
-        isLaneFull = new boolean[_numOfLanes];
-        Arrays.fill(isLaneFull, Boolean.FALSE);
+        laneFull = false;
         bed = new Bed(90, 90);
         stopEngine();
     }
 
     /**
      * @param item The vehicle or object you want to load to the ferry.
-     * @param lane Which lane you want to add your item to.
-     *             If full it tries to add to the other. Else full.
      */
 
-    public boolean load(IFerry item, int lane) {
+    public boolean load(IFerry item) {
         if (bed.getAngle() == 0 && currentSpeed == 0) {
-            if (lane < numOfLanes) {
-                if (lanes.get(lane).size() < maxLaneLength) {
-                    lanes.get(lane).add(item);
-                    checkLane(lane);
-                    return true;
-                } else {
-                    for (int i = 0; i < isLaneFull.length; i++) {
-                        if (!isLaneFull[i]) {
-                            lanes.get(i).add(item);
-                            checkLane(i);
-                            return true;
-                        }
-                        if (i == isLaneFull.length - 1) {
-                            System.out.println("Ferry is fully loaded");
-                            return false;
-                        }
-                    }
-                }
-            }
-        } else {
-            System.out.println("Bed is not lowered.");
-            return false;
+            return addToLane(item);
         }
         return false;
     }
 
     /**
-     * @param lane Removes item/vehicle at chosen lane.
+     * Removes item/vehicle from the lane.
      */
-    public boolean unload(int lane) {
+    public boolean unload() {
         if (bed.getAngle() == 0 && currentSpeed == 0) {
-            if (lane <= numOfLanes) {
-                if (lanes.get(lane).size() > 0) {
-                    IFerry item = lanes.get(lane).peek();
-                    item.setPosX(this.posX);
-                    item.setPosY(this.posY);
-                    lanes.get(lane).remove();
-                    checkLane(lane);
-                    return true;
-                } else {
-                    System.out.println("Lane is empty!");
-                    return false;
-                }
-            } else {
-                System.out.println("Lane doesn't exist.");
-                return false;
-            }
-        } else {
-            System.out.println("Bed is not lowered.");
-            return false;
+            return removeFromLane();
         }
         return false;
     }
 
-    //Use to check if lane is full and set isLaneFull to true or false if lane is full or not.
-    private void checkLane(int lane) {
-        isLaneFull[lane] = maxLaneLength <= lanes.get(lane).size();
+    //Helper method to unload. checks if lane has an item and then removes it.
+    private boolean removeFromLane() {
+        if (lane.size() > 0) {
+            lane.remove();
+            checkLane();
+            return true;
+        }
+        return false;
     }
 
-    public int getMaxLaneLength () {
+    //Helper method to load. Checks lane and adds item to lane then checks size again.
+    private boolean addToLane(IFerry item) {
+        if (!checkLane()) {
+            lane.add(item);
+            checkLane();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Use to check if lane is full and set isLaneFull to true or false if lane is full or not.
+    private boolean checkLane() {
+        return laneFull = lane.size() >= maxLaneLength;
+    }
+
+    public int getMaxLaneLength() {
         return maxLaneLength;
     }
 
-    public int getNumOfLanes () {
-        return numOfLanes;
-    }
-
-    public boolean[] getIsLaneFull () {
-        return isLaneFull;
+    public boolean getLaneFull() {
+        return laneFull;
     }
 
     @Override

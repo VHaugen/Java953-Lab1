d54d51a785379a5df9a175a125fe86155d251996
BjornRo <shovitswe@gmail.com>
2020-02-07 15:15:37 +0100

    Saab updated

--- src/Saab95.java
+++ src/Saab95.java
@@ -1,19 +1,13 @@
 import java.awt.*;
 
 public class Saab95 extends PersonCar {
-    private boolean turboOn;
-    //protected TurboEngine engine = new TurboEngine(125);
 
     /**
      * Creates a custom standard <b>Saab 95</b>.
      */
-    public Saab95(Engine engine) {
-
-        super(2,engine , Color.red, "Saab95");
-
+    public Saab95() {
+        super(2, new TurboEngine(200), Color.red, "Saab95");
     }
-
-
 
     /**
      * Increases total speed depending on value entered.
@@ -22,7 +16,8 @@ public class Saab95 extends PersonCar {
      */
     @Override
     protected void incrementSpeed(double amount) {
-        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,engine.getEnginePower() );
+        getMotion().setSpeed(
+                Math.min(getCurrentSpeed() + speedFactor() * amount, getEnginePower()));
     }
 
     /**
@@ -32,7 +27,7 @@ public class Saab95 extends PersonCar {
      */
     @Override
     protected void decrementSpeed(double amount) {
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
+        getMotion().setSpeed(
+                Math.max(getCurrentSpeed() - speedFactor() * amount, 0));
     }
-
 }

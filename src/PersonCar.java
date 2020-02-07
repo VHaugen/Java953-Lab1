8a17cfda13a74e39c07fc45d833f2f59f3e848a2
BjornRo <shovitswe@gmail.com>
2020-02-07 15:07:26 +0100

    Fixed PersonCar

--- src/PersonCar.java
+++ src/PersonCar.java
@@ -5,16 +5,15 @@ public abstract class PersonCar extends Motorized implements ITruckCargo {
 
     /**
      * This constructor is generic and is made to take standard arguments
-     * to specify only the neccasery arguments for creating a car.
-     * OR SO IM TOLD?!?!?!
+     * to specify only the necessary arguments for creating a car.
      *
      * @param nrDoors     The number of doors of this <code>Car</code>.
-     * @param engine The power of the engine in BHP.
+     * @param motor The power of the engine in BHP.
      * @param color       The <code>Color</code> of this <code>Car</code>.
      * @param modelName   The model name of this <code>Car</code>
      */
-    PersonCar(int nrDoors, Engine engine, Color color, String modelName) {
-        super(engine, color, modelName);
+    PersonCar(int nrDoors, Motor motor, Color color, String modelName) {
+        super(motor, color, modelName);
         this.nrDoors = nrDoors;
     }
 

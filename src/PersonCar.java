d87b77ebc11a94a90a22e8377cd87202511a7532
BjornRo <shovitswe@gmail.com>
2020-02-07 14:11:26 +0100

    Removed PersonCar and Renamed Car to PersonCar

--- src/PersonCar.java
+++ src/PersonCar.java
@@ -1,18 +1,30 @@
 import java.awt.*;
 
-abstract public class PersonCar extends Car implements ICarTransport {
+public abstract class PersonCar extends Motorized implements ITruckCargo {
+    protected int nrDoors; // Number of doors on the car
+
     /**
      * This constructor is generic and is made to take standard arguments
      * to specify only the neccasery arguments for creating a car.
      * OR SO IM TOLD?!?!?!
      *
-     * @param _nrDoors     The number of doors of this <code>Car</code>.
+     * @param nrDoors     The number of doors of this <code>Car</code>.
      * @param engine The power of the engine in BHP.
-     * @param _color       The <code>Color</code> of this <code>Car</code>.
-     * @param _modelName   The model name of this <code>Car</code>
+     * @param color       The <code>Color</code> of this <code>Car</code>.
+     * @param modelName   The model name of this <code>Car</code>
      */
-    PersonCar(int _nrDoors, Engine engine, Color _color, String _modelName) {
-        super(_nrDoors, engine, _color, _modelName);
+    PersonCar(int nrDoors, Engine engine, Color color, String modelName) {
+        super(engine, color, modelName);
+        this.nrDoors = nrDoors;
+    }
+
+    /**
+     * Gets the number of doors
+     *
+     * @return The number of doors
+     */
+    public int getNrDoors() {
+        return nrDoors;
     }
 
 }

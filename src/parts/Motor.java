4f44f708465ad381f2f7fb173c8df65df06e5928
BjornRo <shovitswe@gmail.com>
2020-02-07 14:20:25 +0100

    Changed motor structure

--- src/parts/Motor.java
+++ src/parts/Motor.java
@@ -5,14 +5,16 @@ public abstract class Motor {
         this.enginePower = enginePower;
     }
 
-    abstract protected double speedFactor();
-
     public int getEnginePower() {
         return enginePower;
     }
 
     public void setEnginePower(int enginePower) {
         this.enginePower = enginePower;
+    }
+
+    protected double speedFactor() {
+        return enginePower * 0.01f;
     }
 }
 

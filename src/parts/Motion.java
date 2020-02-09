e7f8fdc1b2fd43e22be1ad631c50ede5c0fc5b8b
Concrematio <the_proffs@yahoo.se>
2020-02-09 17:01:34 +0100

    Added comments to Motion.

--- src/parts/Motion.java
+++ src/parts/Motion.java
@@ -8,6 +8,14 @@ public class Motion {
     private double speed;
     private int theta;
 
+    /**
+     *  A Motion in a two dimensional plane. Consists of a Position a velocity and a speed.
+     *  starts with the velocity (x=0,y=1).
+     *
+     * @param posX  The initial x-coordinate of the <code>Position</code>.
+     * @param posY  The initial y-coordinate of the <code>Position</code>.
+     * @param speed The initial speed.
+     */
     public Motion(double posX, double posY, double speed) {
         pos = new Position(posX, posY);
         velX = 0;
@@ -16,51 +24,104 @@ public class Motion {
         this.speed = speed;
     }
 
+    /**
+     * Gets the amount of velocity in the x-direction.
+     * @return The amount of velocity in the x-direction.
+     */
     public double getVelX() {
         return velX;
     }
 
+    /**
+     * Gets the amount of velocity in the y-direction.
+     * @return The amount of velocity in the y-direction.
+     */
     public double getVelY() {
         return velY;
     }
 
+    /**
+     *  Gets the current speed.
+     * @return The current speed.
+     */
     public double getSpeed() {
         return speed;
     }
 
+    /**
+     *  Gets <code>Position</code>.
+     *
+     * @return The current <code>Position</code>.
+     */
     public Position getPos() {
         return pos;
     }
 
+    /**
+     *  Sets <code>Position</code>.
+     *
+     * @param pos The new <code>Position</code>.
+     */
     public void setPos(Position pos) {
         pos.setPos(pos);
     }
 
+    /**
+     *  Gets The current x-coordinate of the <code>Position</code>.
+     *
+     * @return The current x-coordinate of the <code>Position</code>.
+     */
     public double getPosX() {
         return pos.getX();
     }
 
+    /**
+     *  Sets the x-coordinate to a given value.
+     *
+     * @param posX The new x-coordinate
+     */
     public void setPosX(double posX) {
         pos.setX(posX);
     }
 
+    /**
+     *  Gets The current y-coordinate of the <code>Position</code>.
+     *
+     * @return The current y-coordinate of the <code>Position</code>.
+     */
     public double getPosY() {
         return pos.getY();
     }
 
+    /**
+     *  Sets the y-coordinate to a given value.
+     *
+     * @param posY The new y-coordinate
+     */
     public void setPosY(double posY) {
         pos.setY(posY);
     }
 
+    /**
+     *  Sets the speed to a new given value.
+     * @param speed The new speed
+     */
     public void setSpeed(double speed) {
         this.speed = speed;
     }
 
+    /**
+     *  Updates the <code>Position</code> according to the current speed and velocity.
+     */
     public void move() {
         setPosX(getPosX() + velX * speed);
         setPosY(getPosY() + velY * speed);
     }
 
+    /**
+     *  Turns the velocity anti clockwise according to the given degree.
+     * @param degrees The amount of turning in degrees.
+     */
     public void turn(double degrees) {
         theta += degrees;
         theta %= 360;

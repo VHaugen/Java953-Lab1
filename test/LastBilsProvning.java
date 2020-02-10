46f202af8714391bedfa74d17b49a051095a9068
BjornRo <shovitswe@gmail.com>
2020-02-10 15:30:08 +0100

    Added raise and lower tests

--- test/LastBilsProvning.java
+++ test/LastBilsProvning.java
@@ -6,16 +6,43 @@ import static org.junit.Assert.*;
 
 public class LastBilsProvning {
 
+    Ramp ramp;
     Scania scania;
     CargoTransporter cTransport;
     RepairCenter<Scania> repCenter = new RepairCenter<>(2 );
+
     @Before
     public void start()
     {
+        ramp = new Ramp(70);
         scania = new Scania();
         //ferry = new Ferry(200,Color.PINK,"Nova Star",Integer.MAX_VALUE,3);
         cTransport = new CarTransporter(Color.BLACK,"LastBIl");
     }
+
+    @Test
+    public void testRampLower() {
+        //int before = ramp.getAngle();
+        for (int i = 0; i < ramp.getMaxAngle()+1; i++) {
+            ramp.lower();
+        }
+        assertEquals(ramp.getAngle(), ramp.getMaxAngle());
+    }
+
+    @Test
+    public void testRampRaise() {
+        for (int i = 0; i < ramp.getMaxAngle()+1; i++) {
+            ramp.lower();
+        }
+        int before = ramp.getAngle();
+        for (int i = 0; i < ramp.getMaxAngle()+1; i++) {
+            ramp.raise();
+        }
+        assertEquals(ramp.getAngle(), 0);
+        assertTrue(ramp.getAngle() != before);
+    }
+
+
 
     @Test
     public void testScaniaBedRaise()

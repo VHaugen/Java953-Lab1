ee331c7168fd39fe2c92e2111d78fc6f24ba699c
Concrematio <the_proffs@yahoo.se>
2020-02-12 16:29:39 +0100

    Fulfix volvoImage

--- src/GUI/DrawPanel.java
+++ src/GUI/DrawPanel.java
@@ -29,11 +29,11 @@ public class DrawPanel extends JPanel{
         try {
             // You can remove the "pics" part if running outside of IntelliJ and
             // everything is in the same main folder.
-            // volvoImage = ImageIO.read(new File("Volvo240.jpg"));
+            volvoImage = ImageIO.read(new File("src/pics/Volvo240.jpg"));
 
             // Rememember to rightclick src New -> Package -> name: pics -> MOVE *.jpg to pics.
             // if you are starting in IntelliJ.
-            volvoImage = ImageIO.read(DrawPanel.class.getResourceFromStream("pics/Volvo240.jpg"));
+            // volvoImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Volvo240.jpg"));
         } catch (IOException ex)
         {
             ex.printStackTrace();

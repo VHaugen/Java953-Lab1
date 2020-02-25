312b76af5b800d1ffaba9f8fbbceb7bfe3781a69
BjornRo <54636059+BjornRo@users.noreply.github.com>
2020-02-25 16:40:28 +0100

    New branch for refactoring the view model

---
+++ src/GUI/MainView.java
@@ -0,0 +1,102 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class MainView extends JFrame implements ISignalObserver {
+    private final int screenWidth;
+    private final int screenHeight;
+    private int gasAmount = 0;
+
+    DrawPanel drawPanel;
+
+    // Constructor
+    public MainView(String framename, DrawPanel drawPanel, int screenWidth, int screenHeight) {
+        this.drawPanel = drawPanel;
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+        initComponents(framename);
+    }
+
+    private void initComponents(String title) {
+
+        this.setTitle(title);
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+
+        this.add(drawPanel);
+
+        // Make the frame pack all it's components by respecting the sizes if possible.
+        this.pack();
+
+        // Get the computer screen resolution
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        // Center the frame
+        this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+        // Make the frame visible
+        this.setVisible(true);
+        // Make sure the frame exits when "x" is pressed
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public int getGasAmount() {
+        return 0;
+    }
+
+    @Override
+    public void setGasAction(ActionListener e) {
+
+    }
+
+    @Override
+    public void setBrakeAction(ActionListener e) {
+
+    }
+
+    @Override
+    public void setTurboOnAction(ActionListener e) {
+
+    }
+
+    @Override
+    public void setTurboOffAction(ActionListener e) {
+
+    }
+
+    @Override
+    public void raiseRampAction(ActionListener e) {
+
+    }
+
+    @Override
+    public void lowerRampAction(ActionListener e) {
+
+    }
+
+    @Override
+    public void startEngineAction(ActionListener e) {
+
+    }
+
+    @Override
+    public void stopEngineAction(ActionListener e) {
+
+    }
+
+    @Override
+    public void actOnAction() {
+        this.repaint();
+    }
+}
